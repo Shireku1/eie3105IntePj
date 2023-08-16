280c184a30206b9a4b7970eb740e964bccba1969
Shireku1 <141793403+Shireku1@users.noreply.github.com>
2023-08-16 22:39:04 +0800

    Add files via upload

---
+++ Lab 1/lab 1b/lab 1b/main.c
@@ -0,0 +1,45 @@
+/*
+ * lab 1b.c
+ *
+ * Created: 26/9/2022 4:56:51 pm
+ * Author : 20063241d
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    DDRD=0xff;
+	DDRB=0x00;
+
+	int bs =0;					//int represent state
+
+    while (1) 
+    {
+		if ((PINB&(1<<0)) != 0)			//change state
+		{
+		
+		if (bs==1)
+		{bs=0;} 
+
+		else
+		{bs=1;}
+
+		}
+
+		if (bs==1)						//output according state
+		{
+			PORTD=0xff;
+		}
+
+		if (bs==0)
+		{
+			PORTD=0x00;
+		}
+
+
+		for (int i=0; i<1000; i++)		//cool down
+		{for (int j=0; j<1000; j++);}
+    }
+}
+
